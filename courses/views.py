2c4a64994ff5b669cfb7b8e127d0352b4d8cff93
simoncampos1022 <simon.campos1022@gmail.com>
2024-11-30 23:58:06 +0300

    Update (core): create cms

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,60 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.contrib.auth.mixins import (
+    LoginRequiredMixin,
+    PermissionRequiredMixin
+)
+
+from .models import Course
 
 # Create your views here.
+
+
+class OwnerMixin:
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+
+class OwnerEditMixin:
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+
+class OwnerCourseMixin(
+    OwnerMixin, LoginRequiredMixin, PermissionRequiredMixin
+):
+    model = Course
+    fields = ['subject', 'title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerEditMixin, OwnerCourseMixin):
+    template_name = 'courses/manage/course/form.html'
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+    permission_required = 'courses.view_course'
+
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner = self.request.user)
+
+class CourseCreateView(OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(OwnerCourseEditMixin, CreateView):
+    permission_required = 'courses.change_course'
+
+
+class CourseDeleteView(OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/course/delete.html'
+    permission_required = 'courses.delete_course'
+    
+
+
