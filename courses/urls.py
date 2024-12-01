ce6bfc7db3072fc47385e1d901fc0d8de8543883
vuthienbao345 <vuthienbao345@gmail.com>
2024-12-01 09:34:09 +0300

    Update (feat): display catalog of courses

--- courses/urls.py
+++ courses/urls.py
@@ -22,4 +22,14 @@ urlpatterns = [
         views.CourseDeleteView.as_view(),
         name = 'course_delete'
     ),
+    path(
+        'subject/<slug:subject>/',
+        views.CourseListView.as_view(),
+        name = 'course_list_subject'
+    ),
+    path(
+        '<slug:slug>/',
+        views.CourseDetailView.as_view(),
+        name = 'course_detail'
+    ),
 ]
