43146cb75ec0a6df70929be8843806cf0f2bde28
simoncampos1022 <simon.campos1022@gmail.com>
2024-12-01 09:34:09 +0300

    Update (feat): display catalog of courses

--- courses/views.py
+++ courses/views.py
@@ -1,13 +1,16 @@
 from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView, DeleteView, UpdateView
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404 ,render
 from django.views.generic.list import ListView
 from django.contrib.auth.mixins import (
     LoginRequiredMixin,
-    PermissionRequiredMixin
+    PermissionRequiredMixin,
 )
+from django.db.models import Count
+from django.views.generic.base import TemplateResponseMixin, View
+from django.views.generic.detail import DetailView 
 
-from .models import Course
+from .models import Course, Subject
 
 # Create your views here.
 
@@ -55,3 +58,31 @@ class CourseDeleteView(OwnerCourseMixin, DeleteView):
     
 
 
+class CourseListView(TemplateResponseMixin, View):
+    model = Course
+    template_name = 'courses/course/list.html'
+
+    def get(self, request, subject = None):
+        subjects = Subject.objects.annotate(
+            total_courses =Count('courses')
+        )
+        courses = Course.objects.annotate(
+            total_modules =Count('modules')
+        )
+
+
+        if subject:
+            subject = get_object_or_404(subject = subject)
+            courses = courses.filter(subject = subject)
+        return self.render_to_response(
+            {
+                'subjects': subjects,
+                'subject': subject,
+                'courses': courses,
+            }
+        )
+        
+
+class CourseDetailView(DetailView):
+    model = Course
+    template_name = 'courses/course/detail.html'
