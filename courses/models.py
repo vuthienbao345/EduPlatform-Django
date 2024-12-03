a8998d5ff32911ea72e90c53358f4fae378484e1
simoncampos1022 <simon.campos1022@gmail.com>
2024-12-03 11:31:22 +0300

    Update (core): render course contents

--- courses/models.py
+++ courses/models.py
@@ -2,6 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.template.loader import render_to_string
 
 from .fields import OrderField
 
@@ -102,6 +103,13 @@ class ItemBase(models.Model):
     title = models.CharField(max_length = 250)
     created = models.DateTimeField(auto_now_add = True)
     updated = models.DateTimeField(auto_now_add = True) 
+
+
+    def render(self):
+        return render_to_string(
+            f'courses/content/{self._meta.model_name}.html',
+            {'item': self}
+        )
 
     class Meta:
         abstract = True
