7980fcbc728f6c549807d8d1c01cfc2756d26ad1
vuthienbao345 <vuthienbao345@gmail.com>
2024-12-04 10:30:05 +0300

    Update (feat): use cache framework to add cache levels

--- courses/views.py
+++ courses/views.py
@@ -11,7 +11,7 @@ from django.views.generic.base import TemplateResponseMixin, View
 from django.views.generic.detail import DetailView 
 from django.apps import apps
 from django.forms.models import modelform_factory
-from django.contrib import messages   
+from django.core.cache import cache
 
 
 from .models import Course, Module, Content, Subject
@@ -108,17 +108,31 @@ class CourseListView(TemplateResponseMixin, View):
     template_name = 'courses/course/list.html'
 
     def get(self, request, subject = None):
-        subjects = Subject.objects.annotate(
-            total_courses =Count('courses')
-        )
-        courses = Course.objects.annotate(
+
+        subjects = cache.get('all_subjects')
+        if not subjects:
+            subjects = Subject.objects.annotate(
+                total_courses =Count('courses')
+            )
+            cache.set('all_subjects', subjects)
+        all_courses = Course.objects.annotate(
             total_modules =Count('modules')
         )
 
 
         if subject:
             subject = get_object_or_404(Subject, slug = subject)
-            courses = courses.filter(subject = subject)
+            key = f'subject_{subject.id}_courses'
+            courses = cache.get(key)
+            if not courses:
+                courses = all_courses.filter(subject = subject)
+                cache.set(key, courses)
+        
+        else:
+            courses = cache.get('all_courses')
+            if not courses:
+                courses = all_courses
+                cache.set('all_courses', courses)
         return self.render_to_response(
             {
                 'subjects': subjects,
