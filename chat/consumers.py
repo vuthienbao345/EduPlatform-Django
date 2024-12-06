f440f239379e84da4d349b32dc236dccc7808f88
simoncampos1022 <simon.campos1022@gmail.com>
2024-12-06 11:52:28 +0300

    Update (feat): add context to messages to identify the sender by name

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,10 +1,14 @@
 import json
+
 from asgiref.sync import async_to_sync
+
+from django.utils import timezone
 
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
+        self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
 
@@ -26,17 +30,19 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        now = timezone.now()
 
+        # send message to room group
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
+                'user': self.user.username,
+                'datetime': now.isoformat()
             }
         )
 
-        # send message to WebSocket
-        # self.send(text_data = json.dumps({'message': message}))
 
     def chat_message(self, event):
         # send message to websocket
