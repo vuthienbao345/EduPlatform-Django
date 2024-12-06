1a638e997b6b5f714aaae2e5595052d63901619b
vuthienbao345 <vuthienbao345@gmail.com>
2024-12-06 12:03:03 +0300

    Update (core)made communication fully asynchronous

--- chat/consumers.py
+++ chat/consumers.py
@@ -4,36 +4,37 @@ from asgiref.sync import async_to_sync
 
 from django.utils import timezone
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
-class ChatConsumer(WebsocketConsumer):
-    def connect(self):
+
+class ChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user = self.scope['user']
         self.id = self.scope['url_route']['kwargs']['course_id']
         self.room_group_name = f'chat_{self.id}'
 
         # join group
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
         # accept connection
-        self.accept()
+        await self.accept()
 
-    def disconnect(self, close_code):
+    async def disconnect(self, close_code):
         # leave the room group
-        async_to_sync(self.channel_layer.group_discard)(
+        await self.channel_layer.group_discard(
             self.room_group_name, self.channel_name
         )
 
     
     # receive message from WebSocket
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
         now = timezone.now()
 
         # send message to room group
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -44,6 +45,6 @@ class ChatConsumer(WebsocketConsumer):
         )
 
 
-    def chat_message(self, event):
+    async def chat_message(self, event):
         # send message to websocket
-        self.send(text_data = json.dumps(event))
+        await self.send(text_data = json.dumps(event))
