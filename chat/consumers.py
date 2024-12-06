b8460b34ecd601d36d13347ba74c7ad3f4683b00
vuthienbao345 <vuthienbao345@gmail.com>
2024-12-06 10:55:35 +0300

    Update (feat): enable channel layer

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,19 +1,43 @@
 import json
+from asgiref.sync import async_to_sync
+
 from channels.generic.websocket import WebsocketConsumer
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
-        # 
+        self.id = self.scope['url_route']['kwargs']['course_id']
+        self.room_group_name = f'chat_{self.id}'
+
+        # join group
+        async_to_sync(self.channel_layer.group_add)(
+            self.room_group_name, self.channel_name
+        )
+        # accept connection
         self.accept()
 
     def disconnect(self, close_code):
-        pass
+        # leave the room group
+        async_to_sync(self.channel_layer.group_discard)(
+            self.room_group_name, self.channel_name
+        )
 
     
     # receive message from WebSocket
-    def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
 
+        async_to_sync(self.channel_layer.group_send)(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message,
+            }
+        )
+
         # send message to WebSocket
-        self.send(text_data = json.dumps({'message': message}))
+        # self.send(text_data = json.dumps({'message': message}))
+
+    def chat_message(self, event):
+        # send message to websocket
+        self.send(text_data = json.dumps(event))
