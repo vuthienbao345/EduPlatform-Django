ee85c83e9a6ad9480f5a9d4ab7df6a668c33124a
simoncampos1022 <simon.campos1022@gmail.com>
2024-12-09 18:28:09 +0300

    update(core): go live

--- settings/prod.py
+++ settings/prod.py
@@ -7,10 +7,13 @@ ADMINS = [
     ('msomi', 'sebbievilar2@gmail.com'),
 ]
 
+# ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['sebelearn.com', 'www.sebelearn.com']
 
 DATABASES = {
     'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': config('POSTGRES_DB'),
         'USER': config('POSTGRES_USER'),
@@ -23,3 +26,9 @@ DATABASES = {
 REDIS_URL = 'redis://cache:6379'
 CACHES['default']['LOCATION'] = REDIS_URL
 CHANNEL_LAYERS['default']['CONFIG']['hosts'] = [REDIS_URL]
+
+
+# security
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+SECURE_SSL_REDIRECT = True
